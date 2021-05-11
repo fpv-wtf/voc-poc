fd665cb264e3cbaa0da85a4f8c9a22e5d358cffe
Subby <admin@subby.dev>
2021-05-11 13:58:22 -0700

    Auto retry to connect to goggles when script is started. Code cleanup into standard ES6

--- index.js
+++ index.js
@@ -1,99 +1,126 @@
-const usb = require('usb')
-const yargs = require('yargs')
-const fs = require('fs')
+const usb = require("usb")
+const yargs = require("yargs")
+const fs = require("fs")
+const chalk = require("chalk")
 
+// Sleepy time function
+const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const argv = yargs
-    .option('f', {
-        alias: 'file',
-        describe: 'output video feed to file',
-        type: 'string'
-    })
-    .option('o', {
-        alias: 'stdout',
-        describe: 'send video feed to stdout for playback. eg: node index.js -o | ffplay -',
-        type: 'boolean'
-    })
-    .option('s', {
-        alias: 'readsize',
-        describe: 'size in bytes to queue for usb bulk interface reads',
-        default: 512,
-        type: 'integer'
-    })
-    .option('q', {
-        alias: 'queuesize',
-        describe: 'number of polling usb bulk read requests to keep in flight',
-        default: 3,
-        type: 'integer'
-    })
-    .option('v', {
-        alias: 'verbose',
-        describe: 'be noisy - doesn not play well with -o',
-        type: 'boolean'
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+  .option("f", {
+    alias: "file",
+    describe: "output video feed to file",
+    type: "string",
+  })
+  .option("o", {
+    alias: "stdout",
+    describe:
+      "send video feed to stdout for playback. eg: node index.js -o | ffplay -",
+    type: "boolean",
+  })
+  .option("s", {
+    alias: "readsize",
+    describe: "size in bytes to queue for usb bulk interface reads",
+    default: 512,
+    type: "integer",
+  })
+  .option("q", {
+    alias: "queuesize",
+    describe: "number of polling usb bulk read requests to keep in flight",
+    default: 3,
+    type: "integer",
+  })
+  .option("v", {
+    alias: "verbose",
+    describe: "be noisy - doesn not play well with -o",
+    type: "boolean",
+  })
+  .help()
+  .alias("help", "h").argv
 
-var goggles = usb.findByIds("0x2ca3", "0x1f")
+// Main logic, needs async capability to use promises correctly
+const mainFunction = async () => {
+  let goggles = usb.findByIds("0x2ca3", "0x1f")
 
-if(!goggles) {
-    console.error("Goggles USB device not found. Please connect your goggles and restart the script.")
-    process.exit(1)
-}
-goggles.open()
-if(!goggles.interfaces) {
-    console.error("Couldn't open Goggles USB device")
-    process.exit(1)
-}
-var interface = goggles.interface(3)
-interface.claim()
-if(!interface.endpoints) {
-    console.error("Couldn't claim bulk interface")
-    process.exit(1)
-}
+  while (!goggles) {
+    await sleep(2000)
+    console.log(
+      chalk.red(
+        "Goggles USB device not found. Please connect your goggles. ",
+        chalk.yellow("RETRY IN 2 SECONDS.")
+      )
+    )
 
-if(!argv.f && !argv.o) {
-    console.log("warning: no outputs specified")
+    goggles = usb.findByIds("0x2ca3", "0x1f")
+  }
+
+  console.log(chalk.green("GOGGLES FOUND. ATTEMPTING TO CONNECT."))
+
+  goggles.open()
+
+  if (!goggles.interfaces) {
+    console.error(chalk.red("Couldn't open Goggles USB device"))
+    process.exit(1)
+  }
+
+  const interface = goggles.interface(3)
+
+  interface.claim()
+
+  if (!interface.endpoints) {
+    console.error(chalk.red("Couldn't claim bulk interface"))
+    process.exit(1)
+  }
+
+  if (!argv.f && !argv.o) {
+    console.log(chalk.red("Warning: no outputs specified"))
     argv.v = true
+  }
+
+  let fd
+
+  const inpoint = interface.endpoints[1]
+  inpoint.timeout = 100
+  //outpoint.timeout = 200
+
+  const outpoint = interface.endpoints[0]
+  const magic = Buffer.from("524d5654", "hex")
+
+  outpoint.transfer(magic, (error) => {
+    if (error) {
+      console.error(error)
+    }
+
+    console.debug(chalk.green("Send magic bytes"))
+  })
+
+  inpoint.addListener("data", (data) => {
+    if (argv.o) {
+      process.stdout.write(data)
+    }
+
+    if (argv.v) {
+      console.debug(chalk.green(`Received ${data.length} bytes`))
+    }
+
+    if (argv.f) {
+      if (!fd) {
+        fd = fs.openSync(argv.f, "w")
+
+        if (!fd) {
+          console.error(chalk.red(`Couldn't open file: ${argv.f} : ${err}`))
+          process.exit(1)
+        }
+      }
+
+      fs.writeSync(fd, data)
+    }
+  })
+
+  inpoint.addListener("error", (error) => console.error(error))
+
+  inpoint.startPoll(argv.q, argv.s)
 }
 
-var fd
-
-var inpoint = interface.endpoints[1]
-inpoint.timeout = 100
-//outpoint.timeout = 200
-
-var outpoint = interface.endpoints[0]
-var magic = Buffer.from("524d5654", "hex")
-
-outpoint.transfer(magic, function(error) {
-    if(error) {
-        console.error(error)
-    }
-    console.debug("send magic bytes")
-
-})
-inpoint.addListener("data", function(data) {
-    if(argv.o) {
-        process.stdout.write(data)
-    }
-    if(argv.v) {
-        console.debug("received "+data.length+" bytes")
-    }
-    if(argv.f) {
-        if(!fd) {
-            fd = fs.openSync(argv.f, "w")
-            if(!fd) {
-                console.error("couldn't open file "+ argv.f + ": "+err)
-                process.exit(1)
-            }
-        }
-        fs.writeSync(fd, data)
-    }
-})
-inpoint.addListener("error", function(error) {
-    console.error(error)
-})
-inpoint.startPoll(argv.q, argv.s)
-
+// Run the mainFunction
+mainFunction()
