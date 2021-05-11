eef4a70b90635cd553be0ac3865f2970b7e42749
Joonas Trussmann <joonas.trussmann@cooppank.ee>
2021-05-11 19:50:26 +0300

    Added command line options to drive behavior, some basic error handling

--- index.js
+++ index.js
@@ -1,58 +1,99 @@
-var usb = require('usb')
+const usb = require('usb')
+const yargs = require('yargs')
+const fs = require('fs')
+
+
+const argv = yargs
+    .option('f', {
+        alias: 'file',
+        describe: 'output video feed to file',
+        type: 'string'
+    })
+    .option('o', {
+        alias: 'stdout',
+        describe: 'send video feed to stdout for playback. eg: node index.js -o | ffplay -',
+        type: 'boolean'
+    })
+    .option('s', {
+        alias: 'readsize',
+        describe: 'size in bytes to queue for usb bulk interface reads',
+        default: 512,
+        type: 'integer'
+    })
+    .option('q', {
+        alias: 'queuesize',
+        describe: 'number of polling usb bulk read requests to keep in flight',
+        default: 3,
+        type: 'integer'
+    })
+    .option('v', {
+        alias: 'verbose',
+        describe: 'be noisy - doesn not play well with -o',
+        type: 'boolean'
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 var goggles = usb.findByIds("0x2ca3", "0x1f")
-console.log(goggles)
+
+if(!goggles) {
+    console.error("Goggles USB device not found. Please connect your goggles and restart the script.")
+    process.exit(1)
+}
 goggles.open()
-console.log(goggles.interfaces)
+if(!goggles.interfaces) {
+    console.error("Couldn't open Goggles USB device")
+    process.exit(1)
+}
 var interface = goggles.interface(3)
-console.log(interface)
 interface.claim()
+if(!interface.endpoints) {
+    console.error("Couldn't claim bulk interface")
+    process.exit(1)
+}
 
+if(!argv.f && !argv.o) {
+    console.log("warning: no outputs specified")
+    argv.v = true
+}
 
-const fs = require('fs');
+var fd
 
-// specify the path to the file, and create a buffer with characters we want to write
-let path = 'out.bin';
+var inpoint = interface.endpoints[1]
+inpoint.timeout = 100
+//outpoint.timeout = 200
 
-// open the file in writing mode, adding a callback function where we do the actual writing
-fs.open(path, 'w', function(err, fd) {
-    if (err) {
-        throw 'could not open file: ' + err;
+var outpoint = interface.endpoints[0]
+var magic = Buffer.from("524d5654", "hex")
+
+outpoint.transfer(magic, function(error) {
+    if(error) {
+        console.error(error)
     }
-    var inpoint = interface.endpoints[1]
-    inpoint.timeout = 100
+    console.debug("send magic bytes")
 
-
-    var outpoint = interface.endpoints[0]
-    //var magic = Buffer.from("54564d52", "hex")
-    var magic = Buffer.from("524d5654", "hex")
-    console.log(magic)
-
-    outpoint.transfer(magic, function() {
-        console.log("magic written")
-        /*inpoint.transfer(20000, function(error, data){
-            console.log("error", error)
-            console.log("data", data)
-            setTimeout(getData)
-        })*/
-  
-    })
-    inpoint.addListener("data", function(data) {
-        console.log(data)
+})
+inpoint.addListener("data", function(data) {
+    if(argv.o) {
+        process.stdout.write(data)
+    }
+    if(argv.v) {
+        console.debug("received "+data.length+" bytes")
+    }
+    if(argv.f) {
+        if(!fd) {
+            fd = fs.openSync(argv.f, "w")
+            if(!fd) {
+                console.error("couldn't open file "+ argv.f + ": "+err)
+                process.exit(1)
+            }
+        }
         fs.writeSync(fd, data)
-    })
-    inpoint.addListener("error", function(error) {
-        console.log(error)
-    })
-    inpoint.startPoll(3, 20000)
-    
-    // write the contents of the buffer, from position 0 to the end, to the file descriptor returned in opening our file
+    }
+})
+inpoint.addListener("error", function(error) {
+    console.error(error)
+})
+inpoint.startPoll(argv.q, argv.s)
 
-
-});
-
-
-/*endpoint.transfer(512, function(error, data){
-    console.log(error)
-    console.log(data)
-})*/
